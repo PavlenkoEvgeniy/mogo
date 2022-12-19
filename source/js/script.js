80bc868a78e3e7431468028044d981ab6bc3664b
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2022-12-19 19:08:44 +0300

    Made block with slider - working on simple Java Script

--- source/js/script.js
+++ source/js/script.js
@@ -4,12 +4,12 @@ let wedoTitle = document.querySelectorAll('.what-we-do__title');
 
 // Toggle open/close element of accordion by pressing button
 buttonToggle.forEach(function (buttonToggle) {
-  buttonToggle.addEventListener('click', function() {
+  buttonToggle.addEventListener('click', function () {
 
     // Closing all elements
     let title = document.querySelectorAll('.what-we-do__title');
     title.forEach(function (title) {
-      if (!title.classList.contains('what-we-do__title--closed')){
+      if (!title.classList.contains('what-we-do__title--closed')) {
         title.classList.add('what-we-do__title--closed')
       }
     });
@@ -17,10 +17,9 @@ buttonToggle.forEach(function (buttonToggle) {
     //Opening current element
     let parentElem = this.parentNode;
     let contentBlock = parentElem.querySelector('.what-we-do__title');
-    if(contentBlock.classList.contains('what-we-do__title--closed')) {
-    contentBlock.classList.remove('what-we-do__title--closed');
-    }
-    else {
+    if (contentBlock.classList.contains('what-we-do__title--closed')) {
+      contentBlock.classList.remove('what-we-do__title--closed');
+    } else {
       contentBlock.classList.add('what-we-do__title--closed');
     }
 
@@ -29,12 +28,12 @@ buttonToggle.forEach(function (buttonToggle) {
 
 // Toggle open/close element of accordion by pressing title element
 wedoTitle.forEach(function (wedoTitle) {
-  wedoTitle.addEventListener('click', function() {
+  wedoTitle.addEventListener('click', function () {
 
     // Closing all elements
     let title = document.querySelectorAll('.what-we-do__title');
     title.forEach(function (title) {
-      if (!title.classList.contains('what-we-do__title--closed')){
+      if (!title.classList.contains('what-we-do__title--closed')) {
         title.classList.add('what-we-do__title--closed')
       }
     });
@@ -42,12 +41,72 @@ wedoTitle.forEach(function (wedoTitle) {
     //Opening current element
     let parentElem = this.parentNode;
     let contentBlock = parentElem.querySelector('.what-we-do__title');
-    if(contentBlock.classList.contains('what-we-do__title--closed')) {
-    contentBlock.classList.remove('what-we-do__title--closed');
-    }
-    else {
+    if (contentBlock.classList.contains('what-we-do__title--closed')) {
+      contentBlock.classList.remove('what-we-do__title--closed');
+    } else {
       contentBlock.classList.add('what-we-do__title--closed');
     }
-    
+
   });
+});
+
+/* Slider */
+let buttonSliderPrev = document.querySelector('.slider__button--prev');
+let buttonSliderNext = document.querySelector('.slider__button--next');
+let sliderItems = document.querySelectorAll('.slider__item');
+
+// Making move to the next slide
+let nextSlide = function () {
+  for (let i = 0; i < sliderItems.length; i++) {
+
+    // Checking if it is last slide or not
+    if (i < sliderItems.length) {
+      // If it is last slide make button disabled, if not last - continue
+      if (i === sliderItems.length - 2) {
+        buttonSliderNext.setAttribute('disabled', 'disabled');
+      }
+      // Continue
+      if (sliderItems[i].classList.contains('slider__item--current')) {
+        sliderItems[i].classList.remove('slider__item--current');
+        sliderItems[i + 1].classList.add('slider__item--current');
+
+        console.log('Slide forward');
+        return;
+      }
+    }
+  }
+}
+
+// Making move to the previous slide
+let prevSlide = function () {
+  for (let j = sliderItems.length -1; j > 0; j--) {
+
+    // Checking if it is last slide or not
+    if (j < sliderItems.length) {
+      // If it is first slide make button disabled, if not first - continue
+      if (j === 1) {
+        buttonSliderPrev.setAttribute('disabled', 'disabled');
+      }
+      // Continue
+      if (sliderItems[j].classList.contains('slider__item--current')) {
+        sliderItems[j].classList.remove('slider__item--current');
+        sliderItems[j - 1].classList.add('slider__item--current');
+
+        console.log('Slide back');
+        return;
+      }
+    }
+  }
+}
+
+//Event when pressing next button
+buttonSliderNext.addEventListener('click', function () {
+  nextSlide();
+  buttonSliderPrev.removeAttribute('disabled');
+});
+
+//Event when pressing previous button
+buttonSliderPrev.addEventListener('click', function () {
+  prevSlide();
+  buttonSliderNext.removeAttribute('disabled');
 });
