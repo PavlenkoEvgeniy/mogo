fa9c09ca042d216b734046f4439dd13f1968f4dd
PavlenkoEvgeniy <pavlenkoevgeniy85@gmail.com>
2022-12-20 18:24:30 +0300

    Made second slider, reviewed and refubrished HTML and Java Script

--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,5 @@
-/*  Accordion for 'What we do' section */
+/*  Accordion for 'What we do' section
+------------------------------------------------------------------------*/
 let buttonToggle = document.querySelectorAll('.what-we-do__button-toggle');
 let wedoTitle = document.querySelectorAll('.what-we-do__title');
 
@@ -50,10 +51,11 @@ wedoTitle.forEach(function (wedoTitle) {
   });
 });
 
-/* Slider */
-let buttonSliderPrev = document.querySelector('.slider__button--prev');
-let buttonSliderNext = document.querySelector('.slider__button--next');
-let sliderItems = document.querySelectorAll('.slider__item');
+/* Slider
+-----------------------------------------------------------------------*/
+let buttonSliderPrev = document.querySelector('.slider__button--prev-first-slider');
+let buttonSliderNext = document.querySelector('.slider__button--next-first-slider');
+let sliderItems = document.querySelectorAll('.slider__item--first-slider');
 
 // Making move to the next slide
 let nextSlide = function () {
@@ -109,4 +111,66 @@ buttonSliderNext.addEventListener('click', function () {
 buttonSliderPrev.addEventListener('click', function () {
   prevSlide();
   buttonSliderNext.removeAttribute('disabled');
+});
+
+/* Second slider
+------------------------------------------------------------------------*/
+let buttonSliderPrevSecond = document.querySelector('.slider__button--prev-second-slider');
+let buttonSliderNextSecond = document.querySelector('.slider__button--next-second-slider');
+let sliderItemsSecond = document.querySelectorAll('.slider__item--second-slider');
+
+// Making move to the next slide
+let nextSlideSecond = function () {
+  for (let i = 0; i < sliderItemsSecond.length; i++) {
+
+    // Checking if it is last slide or not
+    if (i < sliderItemsSecond.length) {
+      // If it is last slide make button disabled, if not last - continue
+      if (i === sliderItemsSecond.length - 2) {
+        buttonSliderNextSecond.setAttribute('disabled', 'disabled');
+      }
+      // Continue
+      if (sliderItemsSecond[i].classList.contains('slider__item--current')) {
+        sliderItemsSecond[i].classList.remove('slider__item--current');
+        sliderItemsSecond[i + 1].classList.add('slider__item--current');
+
+        console.log('Slide forward');
+        return;
+      }
+    }
+  }
+}
+
+// Making move to the previous slide
+let prevSlideSecond = function () {
+  for (let j = sliderItemsSecond.length -1; j > 0; j--) {
+
+    // Checking if it is last slide or not
+    if (j < sliderItemsSecond.length) {
+      // If it is first slide make button disabled, if not first - continue
+      if (j === 1) {
+        buttonSliderPrevSecond.setAttribute('disabled', 'disabled');
+      }
+      // Continue
+      if (sliderItemsSecond[j].classList.contains('slider__item--current')) {
+        sliderItemsSecond[j].classList.remove('slider__item--current');
+        sliderItemsSecond[j - 1].classList.add('slider__item--current');
+
+        console.log('Slide back');
+        return;
+      }
+    }
+  }
+}
+
+//Event when pressing next button
+buttonSliderNextSecond.addEventListener('click', function () {
+  nextSlideSecond();
+  buttonSliderPrevSecond.removeAttribute('disabled');
+});
+
+//Event when pressing previous button
+buttonSliderPrevSecond.addEventListener('click', function () {
+  prevSlideSecond();
+  buttonSliderNextSecond.removeAttribute('disabled');
 });
